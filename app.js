7f7cfc7cf51ac75dfc20ca8280d744ba0879c539
TyrannosaurusText <ilow@ucdavis.edu>
2021-02-10 20:39:00 -0800

    wrong env name!

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 
 require('dotenv').config();
 const connectionInfo = process.env.NODE_ENV === 'production' ? {
-  connectionString: process.env.PGURL,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 } : {
     user: process.env.PGHOST,
