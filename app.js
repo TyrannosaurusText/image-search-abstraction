25002730d1685db9417a5a112ac702f2bfa1fda8
TyrannosaurusText <ilow@ucdavis.edu>
2021-02-10 19:47:19 -0800

    working locally

--- app.js
+++ app.js
@@ -1,32 +1,125 @@
-const http = require('http');
-const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 const express = require('express')
-
 const app = express()
+const axios = require('axios');
+require('dotenv').config();
+const connectionInfo = process.env.NODE_ENV === 'production' ? {
+  connectionString: process.env.PGURL,
+} : {
+    user: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOSTADDR,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+  }
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const connectToDatabase = () => {
+  try {
+    const client = new Client(connectionInfo);
+    client.connect()
+    return client;
+  }
+  catch (e) {
+    console.log("Could not connect to database.");
+  }
+}
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const maintainTable = () => {
+  const client = connectToDatabase();
+  client.query(`
+  SELECT COUNT(*) FROM recentsearch WHERE id IN (SELECT id FROM recentsearch ORDER BY id DESC LIMIT 100 OFFSET 100);
+  `, (err, result) => {
+    if (err) console.log(err)
+    client.end();
+    if (result.rows[0].count >= 100) {
+      const client = connectToDatabase();
+      client.query(`
+      DELETE FROM recentsearch WHERE id IN (SELECT id FROM recentsearch ORDER BY id DESC LIMIT 100 OFFSET 100);
+      `, (err, result) => {
+        client.end();
+      })
+    }
+  })
+}
 
-app.get('/query', (req, res) => {
+const getImg = (item) => {
+  return {
+    type: item.mime,
+    width: item.image.width,
+    height: item.image.height,
+    size: item.image.byteSize,
+    url: item.link,
+    thumbnail: {
+      url: item.image.thumbnailLink,
+      height: item.image.thumbnailHeight,
+      width: item.image.thumbnailWidth,
+    },
+    description: item.title,
+    parentPage: item.contextLink,
+  };
+};
+
+
+app.listen(port)
+
+app.get('/query/:searchterm', (req, res) => {
+  const url = "https://www.googleapis.com/customsearch/v1?";
+  const key = process.env.GOOGLE_SEARCH_API;
+  const cx = process.env.GOOGLE_SEARCH_ENGINE_ID;
+  const q = req.params.searchterm;
+  const qs = req.query.page || 1;
+  axios({
+    method: "get",
+    url: url,
+    params: {
+      key: key,
+      cx: cx,
+      q: q,
+      searchType: "image",
+      safe: "active",
+      start: 10 * (qs - 1),
+    },
+  }).then(response => {
+    const data = response.data.items;
+    const result = data.map((item) => getImg(item));
+    res.send(JSON.stringify(result, 2));
+
+  }).catch(err => {
+    console.log(err);
+  });
+
+
+  const client = connectToDatabase();
+  client.query(`INSERT INTO recentsearch(query) VALUES ('${q}');`, (err, result) => {
+    client.end();
+    if (err) res.send(err);
+  });
+
+  maintainTable();
 
 })
 const { Client } = require('pg');
 
-app.get('/recent', async (req, res) => {
-  const client = new Client({
-    connectionString: 'postgres://wjmjnyeqafhmiy:30c5f277ccd26388a901871dafcf3764f8dad9a5d99a0b402f2dca5a6362eeb4@ec2-35-174-118-71.compute-1.amazonaws.com:5432/d5794lsus7ddne',
-  })
-  client.connect()
-  client.query('SELECT NOW()', (err, result) => {
-    console.log(err, result);
+app.get('/recent', (req, res) => {
+  const client = connectToDatabase();
+  client.query('SELECT query, timestamp FROM recentsearch;', (err, result) => {
     client.end();
     if (err) res.send(err);
-    res.send(result);
+    res.send(result.rows);
   })
+})
+
+app.get('/makeTable', (req, res) => {
+  const client = connectToDatabase();
+  client.query(`
+  CREATE TABLE IF NOT EXISTS recentsearch (
+    Id serial primary key, 
+    Timestamp timestamptz NOT NULL DEFAULT NOW(), 
+    Query text NOT NULL
+    );`
+    , (err) => {
+      client.end();
+      if (err) res.send(err);
+      res.send("Cannot GET /makeTab");
+    })
 })
